6eda492f23d9e1b493ff3d62bc5bdf0c880e5a24
Karina Macancela <karymacancela@gmail.com>
2019-11-22 14:03:25 -0500

    Implementing socket.io

---
+++ src/Store.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import io from 'socket.io-client'
+
+export const CTX = React.createContext()
+
+const initState = {
+  general: [
+    {from: 'aaron', msg: 'hello'},
+    {from: 'arnold', msg: 'hello'},
+    {from: 'archer', msg: 'hello'}
+  ],
+  topic2: [
+    {from: 'aaron', msg: 'hello'},
+    {from: 'aaron', msg: 'hello'},
+    {from: 'aaron', msg: 'hello'}
+  ]
+}
+
+function reducer(state, action) {
+  const { from, msg, topic } = action.payload
+  switch(action.type) {
+    case 'RECEIVE_MESSAGE':
+      return {
+        ...state,
+        [topic]: [
+          ...state[topic],
+          {
+            from,
+            msg
+          }
+        ]
+      }
+    default:
+      return state
+  }
+}
+
+let socket
+
+function sendChatAction(value) {
+  socket.emit('chat message', value);
+}
+
+export default function Store(props) {
+
+  if (!socket) {
+    socket = io(':3002')
+  }
+
+  const [allChats] = React.useReducer(reducer, initState)
+  return (
+    <CTX.Provider value={ { allChats, sendChatAction } } >
+      { props.children }
+    </CTX.Provider>
+  )
+}
